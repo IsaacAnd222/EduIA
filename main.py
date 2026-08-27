3c01e578f4d5d4bef1f19815386e1251ac5049c1
isaacandrade222 <isaacandrade222@gmail.com>
2026-08-27 00:05:02 -0600

    Crear primera versión del asistente en consola

---
+++ main.py
@@ -0,0 +1,13 @@
+print("====================================")
+print(" ASISTENTE VIRTUAL UNIVERSITARIO")
+print("====================================")
+print("Hola, soy el asistente virtual.")
+
+while True:
+    pregunta = input("Escribe tu pregunta: ")
+
+    if pregunta.lower() == "salir":
+        print("Hasta luego.")
+        break
+
+    print("Estoy procesando tu pregunta...")
